5bf6a42cf53383cb8c3814ed6e9735282d14b3ce
190146 <tejc190146@gmail.com>
2022-06-03 14:22:40 +0900

    push to kadai8

--- src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/BookBean.java
@@ -1,13 +1,15 @@
-package jp.te4a.spring.boot.myapp7;
+package jp.te4a.spring.boot.myapp8;
 
 
 import org.springframework.boot.SpringApplication;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean {
 	Integer id;
 	String title;
