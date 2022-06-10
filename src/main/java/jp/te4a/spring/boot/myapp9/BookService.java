563ed21e14b3b683f9052bb9f5cbfd5684c8f481
190146 <tejc190146@gmail.com>
2022-06-10 12:21:54 +0900

    fix error

--- src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -8,10 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jp.te4a.spring.boot.myapp9.BookBean;
 import jp.te4a.spring.boot.myapp9.BookForm;
 @Service
 public class BookService {
 	@Autowired
+	BookRepository bookRepository;
 	public BookForm create(BookForm bookForm) {
 		BookBean bookBean = new BookBean();
 		BeanUtils.copyProperties(bookForm, bookBean);
@@ -25,14 +27,7 @@ public class BookService {
 		bookRepository.save(bookBean);
 		return bookForm;
 	}
-
-	public void delete(Integer id) {
-
-		BookBean bean = new BookBean();
-		bean.setId(id);
-		bookRepository.delete(bean);
-
-	}
+	public void delete(Integer id) { bookRepository.deleteById(id); }
 	public List<BookForm> findAll() {
 		List<BookBean> beanList = bookRepository.findAll();
 		List<BookForm> formList = new ArrayList<BookForm>();
@@ -43,27 +38,10 @@ public class BookService {
 		}
 		return formList;
 	}
-	//repositoryのデータを取り出す
-	public BookForm findById(Integer id) {			
-
-		//BookForm bookBean = bookRepository.findById(id);
+	public BookForm findOne(Integer id) {
 		Optional<BookBean> bookBean = bookRepository.findById(id);
-
-		//ラムダ関数
-		Optional<BookBean> opt =  bookRepository.findById(id);
-		opt.ifPresent(book -> {
-			// bookを使った処理
-		});
-
 		BookForm bookForm = new BookForm();
 		BeanUtils.copyProperties(bookBean, bookForm);
 		return bookForm;
-
-		/*public BookForm findOne(Integer id) {
-		BookBean bookBean = findById(id);
-		BookForm bookForm = new BookForm();
-		BeanUtils.copyProperties(bookBean, bookForm);
-		return bookForm;
-	}*/
 	}
 }
