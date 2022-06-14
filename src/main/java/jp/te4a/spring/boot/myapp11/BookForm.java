f26cf79f5c81ee9444661c86af3ab54a6f0a7586
190146 <tejc190146@gmail.com>
2022-06-14 10:44:35 +0900

    push to kadai11

--- src/main/java/jp/te4a/spring/boot/myapp11/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/BookForm.java
@@ -1,13 +1,24 @@
-package jp.te4a.spring.boot.myapp9;
+package jp.te4a.spring.boot.myapp11;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
+@Valid
 public class BookForm {
   private Integer id ;
+  @NotNull
+  @Size(min=3)
   private String title;
+  @Size(min=3, max=20)
   private String writter;
   private String publisher;
+  @Min(0)
   private Integer price;
 }
